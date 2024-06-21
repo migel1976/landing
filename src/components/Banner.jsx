bc61b978d45f89335472f340c2075ccfaba9367d
smirnov <smirnov@alfasatcom.ru>
2024-06-21 09:45:50 +0300

    (LAN-28):устранение замечаний по баннеру

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,41 +7,36 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  column-gap: 80px;
+  gap: 80px;
 `;
 
-const LeftBlock = styled.div`
-  padding-top: 80px;
-  height: 520px;
+const TextSection = styled.section`
   width: 600px;
 `;
 
-const RightBlock = styled.div`
-  height: 505px;
-  width: 600px;
-`;
-
-const HeadText = styled.div`
+const HeadArticle = styled.article`
   padding-bottom: 48px;
   font-size: 54px;
   font-weight: 700;
+  text-transform: uppercase;
 `;
-const SubText = styled.div`
+
+const Paragraph = styled.p`
   padding-bottom: 64px;
   font-size: 18px;
-  font-weight: 200;
+  font-weight: 400;
 `;
 
 const Button = styled.button`
   padding: 16px 18px;
   border: 2px solid #458ff6;
-  background: ${({ primary }) => (primary ? "#458ff6" : "")};
+  background: ${({ primary }) => (primary ? "#458ff6" : "transparent")};
   color: ${({ primary }) => (primary ? "#fff" : "#458ff6")};
   margin-right: 16px;
 
   font-size: 20px;
   font-weight: 500;
-  cursor: grab;
+  cursor: pointer;
 `;
 
 const Image = styled.div`
@@ -53,21 +48,19 @@ const Image = styled.div`
 export default function Banner() {
   return (
     <Container>
-      <LeftBlock>
-        <HeadText>ДОКАЗАТЕЛЬНАЯ МЕДИЦИНА ДЛЯ ВСЕХ</HeadText>
-        <SubText>
+      <TextSection>
+        <HeadArticle>доказательная медицина для всех</HeadArticle>
+        <Paragraph>
           Мы еще очень молоды, но достаточно амбициозны и планируем каждого
           случайного гостя нашего сайта превратить в его постоянного
           пользователя.
-        </SubText>
+        </Paragraph>
         <Button primary={+true} onClick={() => alert("Войти как врач")}>
           Войти как врач
         </Button>
-        <Button onClick={() => alert("Подробнее")}>Подробнее </Button>
-      </LeftBlock>
-      <RightBlock>
-        <Image />
-      </RightBlock>
+        <Button onClick={() => alert("Подробнее")}>Подробнее</Button>
+      </TextSection>
+      <Image />
     </Container>
   );
 }

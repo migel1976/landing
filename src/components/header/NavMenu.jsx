01d1521cd5a8029cc501a69e04770139753875ee
Dyadyusha Artem <artikool5@yandex.ru>
2024-06-22 11:41:15 +0300

    feat(LAN-30): добавлен компонент меню навигации

---
+++ src/components/header/NavMenu.jsx
@@ -0,0 +1,94 @@
+import styled from "styled-components";
+import menuIcon from "../../assets/menu-icon.svg";
+
+const menuItems = [
+  { name: "Главная", href: "#main" },
+  { name: "О проекте", href: "#about" },
+  { name: "Статьи", href: "#articles" },
+  { name: "Отзывы", href: "#testemonies" },
+  { name: "Знания", href: "#knowledge-base" },
+  { name: "Вопросы", href: "#faq" },
+  { name: "Цены", href: "#prises" },
+  { name: "Оставить заявку", href: "#contact-form" },
+  { name: "Спонсоры", href: "#sponsors" },
+];
+
+const OuterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  padding-block: ${({ theme }) => theme.spacing.x1};
+  padding-inline: ${({ theme }) => theme.spacing.x10};
+
+  max-width: 1440px;
+
+  background-color: ${({ theme }) => theme.color.coolGray10};
+
+  font: ${({ theme }) => theme.typography.menu};
+  color: ${({ theme }) => theme.color.coolGray90};
+`;
+
+const StyledNavMenu = styled.nav``;
+
+const NavList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  gap: ${({ theme }) => theme.spacing.x1};
+`;
+
+const NavListItem = styled.li`
+  display: flex;
+  align-items: center;
+`;
+
+const NavLink = styled.a`
+  padding: 8px 16px;
+  color: inherit;
+
+  &:focus {
+    outline: 3px solid ${({ theme }) => theme.color.primary60};
+  }
+`;
+
+const NavMenuButton = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+
+  padding: 8px 16px;
+
+  background-color: ${({ theme }) => theme.color.white};
+  border: 1px solid ${({ theme }) => theme.color.coolGray30};
+
+  cursor: pointer;
+
+  &:focus {
+    outline: 3px solid ${({ theme }) => theme.color.primary60};
+  }
+`;
+
+function NavMenu() {
+  return (
+    <OuterContainer>
+      <StyledNavMenu>
+        <NavList>
+          <NavMenuButton tabIndex="1">
+            <img src={menuIcon} />
+            <span>Меню</span>
+          </NavMenuButton>
+          {menuItems.map((item, i) => {
+            return (
+              <NavListItem key={i}>
+                <NavLink href={item.href} tabIndex="1">
+                  {item.name}
+                </NavLink>
+              </NavListItem>
+            );
+          })}
+        </NavList>
+      </StyledNavMenu>
+    </OuterContainer>
+  );
+}
+
+export default NavMenu;

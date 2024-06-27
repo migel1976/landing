0b7adb7ad8fe0c91ed7e040a7e993ace5888a60f
smirnov <smirnov@alfasatcom.ru>
2024-06-27 16:53:22 +0300

    feat(LAN-22) работает next и prev swiper, но есть вопросы

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -4,30 +4,60 @@ import userThumb from "../../assets/user-thumb.png";
 import artVenueIcon from "../../assets/art-venue.png";
 import chevronLeft from "../../assets/chevron-left.png";
 import chevronRight from "../../assets/chevron-right.png";
-import { useRef } from "react";
-import { useCallback } from "react";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { useSwiper } from "swiper/react";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
+const data = [
+  {
+    id: 1,
+    title: "Zoomerr",
+    img: zoomerIcon,
+    text: " гласит, что наличие хороших навыков чтения в детском возрасте является предиктором высокого уровня интеллекта у молодых взрослых людей.",
+    href: "https://medspecial.ru/wiki/Исследование/",
+    hrefText: "Иследование",
+    name: "Петров Петр",
+    occupation: "Невролог",
+    thumb: userThumb,
+  },
+  {
+    id: 2,
+    title: "ArtVenue",
+    img: artVenueIcon,
+    text: " Астма - это хроническое заболевание легких. Симптомы заболевания включают кашель, свистящее дыхание, одышку и чувство стеснения в груди.",
+    href: "",
+    hrefText: "",
+    name: "Кириллов Кирилл",
+    occupation: "Пульмонолог",
+    thumb: userThumb,
+  },
+];
+
 const Container = styled.div`
   background: linear-gradient(180deg, #67c3f3 0%, #5a98f2 100%);
   width: 1440px;
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-inline: 80px;
+    padding-block: 80px;
+    // gap: 5px;
+  }
 `;
 
-const ContainerBlock = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding-inline: 80px;
-  padding-block: 120px;
-  gap: 10px;
-`;
+// const ContainerBlock = styled.div`
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   padding-inline: 80px;
+//   padding-block: 120px;
+//   gap: 10px;
+// `;
 
 const Block = styled.section`
   display: flex;
@@ -80,90 +110,66 @@ const PersonBlock = styled.div`
   }
 `;
 
-const Chevron = styled.div`
+const NavLeft = styled.div`
+  position: absolute;
+  top: 220px;
+  left: 40px;
+
   width: 24px;
   height: 24px;
 `;
 
-const data = [
-  {
-    id: 1,
-    title: "Zoomerr",
-    img: zoomerIcon,
-    text: " гласит, что наличие хороших навыков чтения в детском возрасте является предиктором высокого уровня интеллекта у молодых взрослых людей.",
-    href: "https://medspecial.ru/wiki/Исследование/",
-    hrefText: "Иследование",
-    name: "Петров Петр",
-    occupation: "Невролог",
-    thumb: userThumb,
-  },
-  {
-    id: 2,
-    title: "ArtVenue",
-    img: artVenueIcon,
-    text: " Астма - это хроническое заболевание легких. Симптомы заболевания включают кашель, свистящее дыхание, одышку и чувство стеснения в груди.",
-    href: "",
-    hrefText: "",
-    name: "Кириллов Кирилл",
-    occupation: "Пульмонолог",
-    thumb: userThumb,
-  },
-];
+const NavRight = styled.div`
+  position: absolute;
+  top: 220px;
+  left: 1350px;
+
+  width: 24px;
+  height: 24px;
+`;
 
 const SwiperNavigations = () => {
   const swiper = useSwiper();
 
   return (
-    <div className="navigation-btns">
-      <button onClick={() => swiper.slidePrev()}>PREV</button>
-      <button onClick={() => swiper.slideNext()}>NEXT</button>
-    </div>
+    <>
+      <NavLeft>
+        <img src={chevronLeft} onClick={() => swiper.slidePrev()} />
+      </NavLeft>
+      <NavRight>
+        <img src={chevronRight} onClick={() => swiper.slidePrev()} />
+      </NavRight>
+    </>
   );
 };
 
 const Slider = () => {
-  const swiper = useSwiper();
-
   return (
     <Container>
-      <ContainerBlock>
-        <Swiper spaceBetween={20} slidesPerView={2}>
-          <Chevron className="navigation-btns">
-            {/* почему то не срабатывает swiper :( */}
-            <img src={chevronLeft} onClick={() => swiper.slidePrev()} />
-          </Chevron>
-          {data.map((item) => (
-            <SwiperSlide key={item.id}>
-              <Block>
-                <ImgBlock>
-                  <img src={item.img} />
-                  <span>{item.title}</span>
-                </ImgBlock>
-                <TextBlock>
-                  <a href={item.href} target="_blank">
-                    {item.hrefText}
-                  </a>
-                  {item.text}
-                </TextBlock>
-                <PersonBlock>
-                  <img src={userThumb} />
-                  <h1>{item.name}</h1>
-                  <h4>{item.occupation}</h4>
-                </PersonBlock>
-              </Block>
-            </SwiperSlide>
-          ))}
-
-          {/* <Chevron> */}
-          <Chevron className="navigation-btns">
-            {/* почему то не срабатывает swiper :( */}
-            <img src={chevronRight} onClick={() => swiper.slideNext()} />
-          </Chevron>
-
-          {/* если подключить отдельно этот компонент то срабатывает swiper */}
-          {/* <SwiperNavigations /> */}
-        </Swiper>
-      </ContainerBlock>
+      <Swiper spaceBetween={20} slidesPerView={2} className="container">
+        {data.map((item) => (
+          <SwiperSlide key={item.id}>
+            <Block>
+              <ImgBlock>
+                <img src={item.img} />
+                <span>{item.title}</span>
+              </ImgBlock>
+              <TextBlock>
+                <a href={item.href} target="_blank">
+                  {item.hrefText}
+                </a>
+                {item.text}
+              </TextBlock>
+              <PersonBlock>
+                <img src={userThumb} />
+                <h1>{item.name}</h1>
+                <h4>{item.occupation}</h4>
+              </PersonBlock>
+            </Block>
+          </SwiperSlide>
+        ))}
+        <SwiperNavigations />
+      </Swiper>
     </Container>
   );
 };
